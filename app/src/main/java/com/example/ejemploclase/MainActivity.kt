33037d6b9eec91e2a35d835711c01873eca7be86
MatiasWod <mwodtke@itba.edu.ar>
2022-11-03 12:02:18 -0300

    Login

--- app/src/main/java/com/example/ejemploclase/MainActivity.kt
+++ app/src/main/java/com/example/ejemploclase/MainActivity.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ejemploclase.ui.theme.EjemploClaseTheme
@@ -23,7 +33,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             EjemploClaseTheme {
-                LogInScreen(listOf("World","Compose"))
+                LogInScreen(listOf("World", "Compose"))
             }
         }
     }
@@ -36,25 +46,81 @@ fun LogInScreen(names: List<String>) {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.primary
     ) {
-        Image(painter = painterResource(id = R.drawable.hercules),
-            contentDescription = null,
-            alignment = Alignment.TopEnd,
-            modifier = Modifier.fillMaxWidth().padding(vertical = 50.dp))
-        Row (modifier = Modifier.padding(vertical = 50.dp).sizeIn(minHeight = 200.dp)) {
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxHeight(),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.hercules),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .size(320.dp)
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.herculespalabras),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
+            val username = remember { mutableStateOf(TextFieldValue()) }
+            val password = remember { mutableStateOf(TextFieldValue()) }
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                label = { Text(text = "Username") },
+                value = username.value,
+                onValueChange = { username.value = it },
+                colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.White)
+            )
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                label = { Text(text = "Password") },
+                value = password.value,
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                onValueChange = { password.value = it },
+                colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.White)
+            )
+            Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+                Button(
+                    onClick = { },
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.login),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                }
+            }
 
         }
     }
 }
 
+
 @Composable
 fun Greeting(name: String) {
-    var expanded by remember { mutableStateOf(false)}
-    Row{
-        Column(modifier = Modifier.padding(24.dp)){
+    var expanded by remember { mutableStateOf(false) }
+    Row {
+        Column(modifier = Modifier.padding(24.dp)) {
             Text(text = "Hello, ")
             Text(text = name)
-            OutlinedButton(onClick = { expanded = !expanded}) {
-                Text(if(expanded) "Show less" else "Show more")
+            OutlinedButton(onClick = { expanded = !expanded }) {
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
@@ -64,6 +130,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     EjemploClaseTheme {
-        LogInScreen(listOf("World","Compose"))
+        LogInScreen(listOf("World", "Compose"))
     }
 }
--- app/src/main/java/com/example/ejemploclase/ui/theme/Type.kt
+++ app/src/main/java/com/example/ejemploclase/ui/theme/Type.kt
@@ -2,9 +2,12 @@ package com.example.ejemploclase.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.ejemploclase.R
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -12,7 +15,7 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
@@ -26,3 +29,4 @@ val Typography = Typography(
     )
     */
 )
+
