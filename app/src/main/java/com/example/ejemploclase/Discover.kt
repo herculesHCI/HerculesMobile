7385b14b5444ac6a07e947d2beaaea2be30fa7e1
AndresCarro <acarro@itba.edu,ar>
2022-11-09 13:32:58 -0300

    Cambios en la tarjeta de Workout a medio implementar y Discover es ahora scrolleable.

--- app/src/main/java/com/example/ejemploclase/Discover.kt
+++ app/src/main/java/com/example/ejemploclase/Discover.kt
@@ -3,17 +3,13 @@ package com.example.ejemploclase
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -22,8 +18,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
@@ -34,13 +32,17 @@ fun Discover(){
 
 }
 
+val items = listOf("Diego´s Workout", "Nono´s Workout", "Tute´s Workout", "ABRAKADABRA","Noel Workout")
+val exercises = listOf("Diego´s Workout", "Nono´s Workout", "Tute´s Workout", "ABRAKADABRA","Noel Workout");
+
 
 @Composable
 fun DiscoverContent(){
     Box(modifier = Modifier
         .fillMaxSize()
         .background(Color.White)
-        .padding(horizontal = 25.dp)){
+        .padding(horizontal = 25.dp)
+        .verticalScroll(rememberScrollState())){
         Column(){
             val searchField = remember { mutableStateOf(TextFieldValue()) }
 
@@ -65,20 +67,6 @@ fun DiscoverContent(){
                         Spacer(modifier = Modifier.fillMaxWidth(.85f))
                         Icon(imageVector = Icons.Default.List,
                             /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/
-                            /**CHANGE ICON HERE*/ /**CHANGE ICON HERE*/
                             contentDescription = null,)
 
                     }
@@ -89,16 +77,15 @@ fun DiscoverContent(){
                 onValueChange = { searchField.value = it },
                 colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.White, textColor = Color.Black)
             )
-
-            WorkoutElement(item = "AndosWorkout")
-
+            for(item in items){
+                Row(modifier = Modifier.padding(10.dp)){
+                    WorkoutElement(item = item)
+                }
+            }
         }
 
     }
 }
-
-
-
 
 @Composable
 fun WorkoutElement(item: String) {
@@ -110,28 +97,35 @@ fun WorkoutElement(item: String) {
             .padding(10.dp)
     ){
         Row(){
-
             Column(modifier = Modifier.weight(0.7f)) {
                 Row(){
                     Text(text= item ,
-                        modifier = Modifier
-                            .padding(horizontal = 15.dp)
-                            .defaultMinSize())
-                    Icon(
-                        imageVector = Icons.Default.Star ,
-                        contentDescription = null
-                    )
+                        fontSize = 17.sp)
+                }
+                Text(text= "Upper",
+                    fontSize = 12.sp)
+            }
+            Column(){
+                Row(){
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Star ,
+                            contentDescription = null
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.AddCircle ,
+                            contentDescription = null
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) { // TODO Importar paquete de iconos nuevos
+                        Icon(imageVector = Icons.Default.KeyboardArrowUp,
+                            contentDescription = null)
+                    }
+                    Text(text = "7.3", textAlign =  TextAlign.Justify) // TODO alinear bien
                 }
 
-                Text(text="test\ntest\ntest\ntest\ntest\n" )
-
-            }
-
-            Column(){
-                Icon(
-                    imageVector = Icons.Default.AddCircle ,
-                    contentDescription = null
-                )
             }
 
         }
